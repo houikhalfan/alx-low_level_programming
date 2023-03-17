8045982da91aab3d348c953be27cc303331c6eb6
houikhalfan <h.ouikhalfan@gmail.com>
2023-03-17 16:29:31 -0700

    task 4 again

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,17 @@
 int main(void)
 {
 char abc;
-
 for (abc = 'a'; abc <= 'z'; abc++)
 {
-if (abc != 'q' || abc != 'e')
+if (abc == 'q' || abc == 'e')
 {
-putchar(abc);
+    abc++;
+
 }
 else
 {
-abc ++;	
-}	
+ putchar(abc);
+}
 }
 putchar('\n');
 return (0);
