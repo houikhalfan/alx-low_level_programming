11d1bdbdcfb1a195709b2688234df9b2060168eb
houikhalfan <h.ouikhalfan@gmail.com>
2023-03-22 15:51:56 -0700

    task 5 again3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,12 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-}
-else if (n < 0) 
+}else if (n < 0) 
 {
 _putchar('-');
 return (-1);
-}
-else if (n == 0)  
+}else if (n == 0)  
 {
 _putchar('0');
-}
-return (0); 
+}return (0);
 }
