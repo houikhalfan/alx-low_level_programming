84930abd06a924e11668a9c8bbdfa03722db9592
houikhalfan <h.ouikhalfan@gmail.com>
2023-03-21 17:30:40 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alpha in lowercase
+ */
+void print_alphabet(void)
+{
+char abcs;
+for(abcs='a';abcs='z';abcs++)
+{
+_putchar(abcs);	
+}	
+_putchar('\n');
+}	
