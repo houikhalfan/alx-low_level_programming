35fe7c113dbb57248bca8978b6e3d0761bdcc0ec
houikhalfan <h.ouikhalfan@gmail.com>
2023-03-30 09:17:23 -0700

    task01

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,15 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
-i = 0;
+i = 0, j=0;
 
 while (dest[i] != '\0')
 i++;
-for (j = 0 ; j < n ; j++)
+while (j < n && src[j] != '\0')
 {
 dest[i] = dest[i] + src[j];
 i++;
+j++;
 }
 return (dest);
 }
