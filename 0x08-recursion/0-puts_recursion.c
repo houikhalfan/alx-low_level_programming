891e2c4abb1fae0f0441cc730dc8c761e5d3061c
houikhalfan <h.ouikhalfan@gmail.com>
2023-04-05 15:10:31 -0700

    task00

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-int n = 0;
-while (s[n] != '\0')
+if (*s == '\0')
 {
-_putchar(s[n]);
-n++;
+return;
 }
-_putchar('\n');
+_putchar(*s);
+_puts_recursion(*s++);
 }
