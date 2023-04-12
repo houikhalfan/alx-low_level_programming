acd39c66a9ae84b6ba41c10822c6fc6afcc3ea75
houikhalfan <h.ouikhalfan@gmail.com>
2023-04-12 16:21:28 -0700

    task00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i = 0;
 char *arr;
-arr = malloc(size * sizeof(char));
+arr = malloc((size + 1) * sizeof(char));
 if (size == 0)
 return (NULL);
 while (i < size)
