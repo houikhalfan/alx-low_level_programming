17a615ac0879c51c1b6995940b17390900aa1af6
houikhalfan <h.ouikhalfan@gmail.com>
2023-04-17 09:04:11 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * dog - 
+ * @name: input 
+ * @age: input
+ * @owner: input
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
