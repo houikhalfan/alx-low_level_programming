ded33c6e1474d42c29e8c78209669f245f396a9e
houikhalfan <h.ouikhalfan@gmail.com>
2023-04-19 16:45:42 -0700

    task 1wq

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a fct that executes a function
+ * @array: input
+ * @size: input
+ * @action: input
+ */
+
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array != NULL && action != NULL)
+for (i = 0; i < size; i++)
+action(*(array + 1));
+}
