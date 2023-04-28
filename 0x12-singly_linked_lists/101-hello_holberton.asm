6a2cf65b546759d1adc2c79d408b4e543f7bec50
houikhalfan <h.ouikhalfan@gmail.com>
2023-04-27 17:12:56 -0700

    last

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,15 @@
-global main
 extern printf
+section .text
+   global main
 main:
-mov edi, format
-xor, eax
+push rbp
+mov rdi,fmt
+mov rsi,msg
+mov rax,0
 call printf
-mov exa, 0
+pop rbp
+mov rax,0
 ret
-format: db 'Hello, Holberton\n',0
+section .data
+msg: db 'Hello, Holberton',0
+fmt: db "%s", 10, 0
