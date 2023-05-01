b2c1b75f7cb9277722aeb51af6c74911dae51fb7
houikhalfan <h.ouikhalfan@gmail.com>
2023-04-30 18:36:28 -0700

    task3

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free llist
+ * @h: pointer to the first node
+ * Return: length
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+listint_t *t;
+size_t l = 0;
+int d;
+if (!h || !*h)
+return (0);
+while (*h)
+{
+d = *h - (*h)->next;
+if (d > 0)
+{
+t = (*h)->next;
+free(*h);
+*h = t;
+l++;
+}
+else
+{
+free(*h);
+*h = NULL;
+l++;
+break;
+}
+}
+*h = NULL;
+return (len);
+}
