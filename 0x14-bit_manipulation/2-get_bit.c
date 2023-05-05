6feb7a31944417f3731c9fec8b9788f656b94b57
houikhalfan <h.ouikhalfan@gmail.com>
2023-05-05 13:49:04 -0700

    task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - a function that returns the value of a bit at a given index.
+ * @n: number
+ * @index: index within binary number
+ * Return: bit 0 or 1, or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+int bit;
+unsigned int max_bi;
+max_bi = (sizeof(unsigned kong int) * 8);
+if (index > max_bi)
+return (-1);
+bit = ((n >> index) & 1);
+return (bit);
+}
