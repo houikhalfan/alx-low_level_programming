365186665bd426bdb2e3ea5b0ce65a1da03ba8a7
houikhalfan <h.ouikhalfan@gmail.com>
2023-11-04 01:16:45 -0700

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,14 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
+hash_table_t *table;
+table = malloc(sizeof(hash_table_t));
 if (table == NULL)
 return (NULL);
 table->size = size;
 table->array = calloc(size, sizeof(hash_node_t *));
-if (table->array == NULL) {
+if (table->array == NULL)
+{
 free(table);
 return NULL;
 }
