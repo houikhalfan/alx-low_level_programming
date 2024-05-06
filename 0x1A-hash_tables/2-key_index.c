ca0fa0b6e5eba71e0bd791b750de2e8992938a9c
houikhalfan <h.ouikhalfan@gmail.com>
2024-05-06 15:01:05 -0700

    nnnew

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,3 +12,4 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	index = (hash_djb2(key)) % size;
 	return (index);
 }
+
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,3 +42,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[idx] = n_d;
 	return (1);
 }
+
